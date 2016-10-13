63960e260b4b0ddd0436ae6943a6164525a3b726
Tomas Harkema <tomas@harkema.in>
2016-10-13 18:48:21 +0200

    Initial Commit

---
+++ AEXCode/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AEXCode
+//
+//  Created by Tomas Harkema on 13-10-16.
+//  Copyright © 2016 Tomas Harkema. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ AEXCode/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AEXCode
+//
+//  Created by Tomas Harkema on 13-10-16.
+//  Copyright © 2016 Tomas Harkema. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
